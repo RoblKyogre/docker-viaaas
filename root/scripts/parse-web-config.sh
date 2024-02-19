99dba5d2757bbcde3a4d6b46e82916766100521b
RoblKyogre <roblkyogre@craftingcomrades.net>
2024-02-19 00:13:25 -0800

    Fix client id parsing

--- root/scripts/parse-web-config.sh
+++ root/scripts/parse-web-config.sh
@@ -8,7 +8,7 @@ touch ${WEB_CONFIG_FILE}
 cat /dev/null >| ${WEB_CONFIG_FILE}
 
 # defaults lifted from https://github.com/ViaVersion/VIAaaS/blob/master/src/main/resources/web/js/config.js
-[[ -z "${AZURE_CLIENT_ID}" ]] && parsedClientId='a370fff9-7648-4dbf-b96e-2b4f8d539ac2' || parsedClientId="${DEPLOY_ENV}"
+[[ -z "${AZURE_CLIENT_ID}" ]] && parsedClientId='a370fff9-7648-4dbf-b96e-2b4f8d539ac2' || parsedClientId="${AZURE_CLIENT_ID}"
 [[ -z "${WHITELISTED_ORIGINS}" ]] && parsedOrigins='https://via.re.yt.nom.br' || parsedOrigins="${WHITELISTED_ORIGINS}"
 [[ -z "${CORS_PROXY}" ]] && parsedProxy='https://cors.re.yt.nom.br/' || parsedProxy="${CORS_PROXY}"
 [[ -z "${INSTANCE_SUFFIX}" ]] && parsedSuffix='null' || parsedSuffix="\"${INSTANCE_SUFFIX}\""
